9847ab94c03a19e99dcbfa667c031e1e3f39a07f
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-11-30 08:05:12 +0530

    backend setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app=express();
+const PORT=3000;
+
+app.get('/',(req,res)=>{
+    res.send("welcome")
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running at PORT:${PORT}`)
+})
