ace5acce91102f12212323935022be8017350e90
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-11-30 09:21:54 +0530

    CommentsSchema defined

---
+++ models/Comments.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Comment", commentSchema);
