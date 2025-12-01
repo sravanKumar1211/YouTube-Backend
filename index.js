2e8224ccd044f250bb395d85206dc45e00ec2220
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-01 11:24:34 +0530

    added CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import cookieParser from "cookie-parser";
 import { connectDB } from "./DataBase/database.js";
 import AuthRoutes from "./Routes/user.routes.js";
@@ -6,6 +7,10 @@ import VideoRoutes from "./Routes/video.routes.js";
 import CommentRoutes from "./Routes/comment.routes.js";
 
 const app = express();
+app.use(cors({
+  origin:'http://localhost:5173/',
+  credentials:true
+}))
 app.use(express.json());
 app.use(cookieParser())
 
