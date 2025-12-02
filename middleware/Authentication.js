0c730004f48470b021590e5ae193a2a2bd9ca471
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-02 16:51:28 +0530

    cors error fixed

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -1,25 +1,3 @@
-
-// import jwt from 'jsonwebtoken';
-// import User from '../models/User.model.js'
-
-// const auth= async(req,res,next)=>{
-//     const token =req.cookies.token;
-//     if(!token){
-//         return res.status(401).json({error:'No token, authorization denied'});
-//     }else{
-//         try{
-//             const decode=jwt.verify(token,"SECRET");
-//             req.user=await User.findById(decode.userId).select('-password');
-//             next();
-//         }catch(err){
-//             res.status(401).json({error:'Token is not valid'});
-//         }
-//     }
-// }
-
-// export default auth;
-
-
 import jwt from "jsonwebtoken";
 import User from "../models/User.model.js";
 
